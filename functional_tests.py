a60ee1cd6188e6a900d73ad4e88f0350be3f63b6
laozhangkanshijie <45612957+laozhangkanshijie@users.noreply.github.com>
2019-07-30 01:16:45 +0800

    要重构测试代码了

--- functional_tests.py
+++ functional_tests.py
@@ -33,22 +33,34 @@ class NewVisitorTest(unittest.TestCase):
 
 
         # 她按回车键后，页面更新了
-        # 代办事项表格中显示了“1：Buy peacock feathers"
+        # 代办事项表格中显示1：Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTure(
-                any(row.text == '1: Buy peacock feathers' for row in rows),
-                "New to-do item did not appear in table"
-                )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
-        # 页面中又显示了一个文本框，可以输入其他的代办事项
+        # 页面中显示了一个文本框，可以输入其他的代办事项
         # 她输入了 “Use peacock feathers to make a fly” （使用孔雀羽毛做假蝇）
         # 伊迪丝做事很有条理
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
+        # 页面再次刷新，清单中显示了这两个待办事项
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly',
+                [row.text for row in rows]
+                )
         # 伊迪丝想知道这个网站是否会记住她的清单
+        # 她按回车键后，页面更新了
+        # 代办事项表格中显示1：Buy peacock feathers"
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
         # 她看到网站为他生成了一个唯一的URL
         # 而且页面中又一些文字解说这个功能
 
