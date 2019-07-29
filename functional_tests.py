f91b39ee933701b621035c94127b9ac3220d43d8
laozhangkanshijie <45612957+laozhangkanshijie@users.noreply.github.com>
2019-07-29 15:39:04 +0800

    functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,19 +17,36 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         # 她注意到网页的标题和头部都包含“To-Do"这个词
-        self.assertIn( 'To-Do', self.browser.title)
-        self.fail('finish the test!')
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIN('To-Do',header_text)
 
         # 应用邀请她输入一个待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item')
 
         # 她在一个文本框中输入了“Buy peacock feathers" (购买孔雀羽毛)
         # 伊迪丝的爱好是使用假蝇做饵料钓鱼
+        inputbox.send_keys('Buy peacock feathers')
+
 
         # 她按回车键后，页面更新了
         # 代办事项表格中显示了“1：Buy peacock feathers"
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTure(
+                any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # 页面中又显示了一个文本框，可以输入其他的代办事项
-
+        # 她输入了 “Use peacock feathers to make a fly” （使用孔雀羽毛做假蝇）
+        # 伊迪丝做事很有条理
+        self.fail('Finish the test!')
         # 伊迪丝想知道这个网站是否会记住她的清单
         # 她看到网站为他生成了一个唯一的URL
         # 而且页面中又一些文字解说这个功能
