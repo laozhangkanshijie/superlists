e8ee9a4fe623fb2256b80e529d75ae614ebcbab1
laozhangkanshijie <45612957+laozhangkanshijie@users.noreply.github.com>
2019-08-02 15:27:45 +0800

    7.3两个用户测试7.3两个用户测试7.3两个用户测试7.3两个用户测试7.3两个用户测试7.3两个用户测试7.3两个用户测试

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 from django.test import LiveServerTestCase
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -11,10 +14,6 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table=self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 伊迪丝听说有一个很酷的在线待办事项应用
@@ -40,30 +39,80 @@ class NewVisitorTest(LiveServerTestCase):
         # 她按回车键后，页面更新了
         # 代办事项表格中显示1：Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        # 页面中显示了一个文本框，可以输入其他的代办事项
+        # 页面中还有一个文本框，可以输入其他的代办事项
         # 她输入了 “Use peacock feathers to make a fly” （使用孔雀羽毛做假蝇）
         # 伊迪丝做事很有条理
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
         
         # 页面再次刷新，清单中显示了这两个待办事项
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # 伊迪丝想知道这个网站是否会记住她的清单
-        # 她按回车键后，页面更新了
-        # 代办事项表格中显示1：Buy peacock feathers"
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.fail('Finish the test!')
-        # 她看到网站为他生成了一个唯一的URL
         # 而且页面中又一些文字解说这个功能
-
+        
         # 她访问那个URL，发现她的待办事项还在
 
         # 她很满意，去睡觉了
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        #伊迪丝新建一个待办事项清单
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # 她看到网站为他生成了一个唯一的URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        # 现在一名叫作弗朗西斯的新用户访问了网站
+
+        ## 我们使用一个新浏览器会话
+        ## 确保伊迪丝的信息不会从cookie中泄露出去
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # 弗朗西斯访问首页
+        # 页面中看不到伊迪丝的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_test)
+        self.assettNotIn('make a fly', page_text)
+
+        # 弗朗西斯输入一个新待办事项， 新建一个清单
+        # 他不像伊迪丝那样兴趣盎然
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.send_keys('Buy milk')
+        inputbox = self.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # 弗朗西斯获得了他的唯一URL
+        francis_lits_url = self.brwser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # 这个页面还是没有伊迪丝的清单
+        page_text= self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # 两个人都很满意，然后睡觉去了
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
